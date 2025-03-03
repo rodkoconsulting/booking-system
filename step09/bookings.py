b801660070eed8de999782b01e196ca115de0d14
Andrew Rodko <arodko@polanerselections.com>
2025-03-03 15:24:12 -0500

    Change references to room and rooms to drone and drones

--- step09/bookings.py
+++ step09/bookings.py
@@ -1,15 +1,14 @@
-#!python3
 import os, sys
 import sqlite3
-from wsgiref.util import setup_testing_defaults, shift_path_info
 from wsgiref.simple_server import make_server
+from wsgiref.util import setup_testing_defaults, shift_path_info
 
 #
 # Ensure we're using the same database filename throughout.
 # It doesn't matter what this is called or where it is:
 # sqlite3 will just accept anything.
 #
-DATABASE_FILEPATH = "bookings.db"
+DATABASE_FILEPATH = "../bookings.db"
 
 def create_database():
     """Connect to the database, read the CREATE statements and split
@@ -63,10 +62,10 @@ def populate_database():
     q.execute(sql, [2, "Donald Duck", "donald.duck@example.com"])
     q.execute(sql, [3, "Kermit the Frog", None])
 
-    sql = "INSERT INTO rooms(id, name, location) VALUES(?, ?, ?)"
-    q.execute(sql, [1, "Room A", "Next to the stairway"])
-    q.execute(sql, [2, "Room B", "On the Second Floor"])
-    q.execute(sql, [3, "Main Hall", None])
+    sql = "INSERT INTO drones(id, name, location) VALUES(?, ?, ?)"
+    q.execute(sql, [1, "Drone A", "South Street Seaport"])
+    q.execute(sql, [2, "Drone B", "Hudson Yards"])
+    q.execute(sql, [3, "Drone C", "UN Building"])
 
     #
     # Triple-quoted strings can cross lines
@@ -76,7 +75,7 @@ def populate_database():
     INSERT INTO
         bookings
     (
-        room_id, user_id, booked_on, booked_from, booked_to
+        drone_id, user_id, booked_on, booked_from, booked_to
     )
     VALUES(
         ?, ?, ?, ?, ?
@@ -110,20 +109,20 @@ def get_users():
     """
     return select("SELECT * FROM users")
 
-def get_rooms():
+def get_drones():
     """Get all the rooms from the database
     """
-    return select("SELECT * FROM rooms")
+    return select("SELECT * FROM drones")
 
 def get_bookings_for_user(user_id):
     """Get all the bookings made by a user
     """
     return select("SELECT * FROM v_bookings WHERE user_id = ?", [user_id])
 
-def get_bookings_for_room(room_id):
+def get_bookings_for_drone(drone_id):
     """Get all the bookings made against a room
     """
-    return select("SELECT * FROM v_bookings WHERE room_id = ?", [room_id])
+    return select("SELECT * FROM v_bookings WHERE drone_id = ?", [drone_id])
 
 def page(title, content):
     """Return a complete HTML page with the title as the <title> and <h1>
@@ -132,7 +131,7 @@ def page(title, content):
     return """
     <html>
     <head>
-    <title>Room Booking System: {title}</title>
+    <title>Drone Booking System: {title}</title>
     <style>
     body {{
         background-colour : #cff;
@@ -140,11 +139,6 @@ def page(title, content):
         padding : 1em;
         border : thin solid black;
         font-family : sans-serif;
-    }}
-    td {{
-        padding : 0.5em;
-        margin : 0.5em;
-        border : thin solid blue;
     }}
 
     </style>
@@ -162,7 +156,7 @@ def index_page(environ):
     html = """
     <ul>
         <li><a href="/users">Users</a></li>
-        <li><a href="/rooms">Rooms</a></li>
+        <li><a href="/drones">Drones</a></li>
         <li><a href="/bookings">Bookings</a></li>
     </ul>
     """
@@ -181,28 +175,28 @@ def users_page(environ):
     html += "</ul>"
     return page("Users", html)
 
-def rooms_page(environ):
+def drones_page(environ):
     """Provide a list of all the rooms, linking to their bookings
     """
     html = "<ul>"
-    for room in get_rooms():
-        html += '<li><a href="/bookings/room/{id}">{name}</a> ({location})</li>\n'.format(
-            id=room['id'],
-            name=room['name'],
-            location=room['location'] or "Location unknown"
+    for drone in get_drones():
+        html += '<li><a href="/bookings/drone/{id}">{name}</a> ({location})</li>\n'.format(
+            id=drone['id'],
+            name=drone['name'],
+            location=drone['location'] or "Location unknown"
         )
     html += "</ul>"
-    return page("Rooms", html)
+    return page("Drones", html)
 
-def bookings_user_page(environ):
+def drones_user_page(environ):
     """Provide a list of bookings by user, showing room and date/time
     """
     user_id = int(shift_path_info(environ))
     html = "<table>"
     html += "<tr><td>Room</td><td>Date</td><td>Times</td></tr>"
     for booking in get_bookings_for_user(user_id):
-        html += "<tr><td>{room_name}</td><td>{booked_on}</td><td>{booked_from} - {booked_to}</td></tr>".format(
-            room_name=booking['room_name'],
+        html += "<tr><td>{drone_name}</td><td>{booked_on}</td><td>{booked_from} - {booked_to}</td></tr>".format(
+            drone_name=booking['drone_name'],
             booked_on=booking['booked_on'],
             booked_from=booking['booked_from'] or "",
             booked_to=booking['booked_to'] or ""
@@ -210,13 +204,13 @@ def bookings_user_page(environ):
     html += "</table>"
     return page("Bookings for user %d" % user_id, html)
 
-def bookings_room_page(environ):
+def bookings_drone_page(environ):
     """Provide a list of bookings by room, showing user and date/time
     """
     room_id = int(shift_path_info(environ))
     html = "<table>"
     html += "<tr><td>User</td><td>Date</td><td>Times</td></tr>"
-    for booking in get_bookings_for_room(room_id):
+    for booking in get_bookings_for_drone(drone_id):
         html += "<tr><td>{user_name}</td><td>{booked_on}</td><td>{booked_from} - {booked_to}</td></tr>".format(
             user_name=booking['user_name'],
             booked_on=booking['booked_on'],
@@ -224,7 +218,7 @@ def bookings_room_page(environ):
             booked_to=booking['booked_to'] or ""
         )
     html += "</table>"
-    return page("Bookings for room %d" % room_id, html)
+    return page("Bookings for drone %d" % drone_id, html)
 
 def bookings_page(environ):
     """Provide a list of all bookings by a user or room, showing
@@ -233,8 +227,8 @@ def bookings_page(environ):
     category = shift_path_info(environ)
     if category == "user":
         return bookings_user_page(environ)
-    elif category == "room":
-        return bookings_room_page(environ)
+    elif category == "drone":
+        return bookings_drone_page(environ)
     else:
         return "No such booking category"
 
@@ -262,8 +256,8 @@ def webapp(environ, start_response):
         data = index_page(environ)
     elif param1 == "users":
         data = users_page(environ)
-    elif param1 == "rooms":
-        data = rooms_page(environ)
+    elif param1 == "drones":
+        data = drones_page(environ)
     elif param1 == "bookings":
         data = bookings_page(environ)
     else:
