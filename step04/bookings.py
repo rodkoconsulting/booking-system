a009c3d63f9451d8adb0d4c83c13ed177fe73cc3
Andrew Rodko <arodko@polanerselections.com>
2025-03-03 13:34:50 -0500

    Replace room references with drones

--- step04/bookings.py
+++ step04/bookings.py
@@ -7,7 +7,7 @@ import sqlite3
 # It doesn't matter what this is called or where it is:
 # sqlite3 will just accept anything.
 #
-DATABASE_FILEPATH = "bookings.db"
+DATABASE_FILEPATH = "../bookings.db"
 
 def create_database():
     """Connect to the database, read the CREATE statements and split
@@ -61,10 +61,10 @@ def populate_database():
     q.execute(sql, [2, "Donald Duck", "donald.duck@example.com"])
     q.execute(sql, [3, "Kermit the Frog", None])
 
-    sql = "INSERT INTO rooms(id, name, location) VALUES(?, ?, ?)"
-    q.execute(sql, [1, "Room A", "Next to the stairway"])
-    q.execute(sql, [2, "Room B", "On the Second Floor"])
-    q.execute(sql, [3, "Main Hall", None])
+    sql = "INSERT INTO drones(id, name, location) VALUES(?, ?, ?)"
+    q.execute(sql, [1, "Drone A", "South Street Seaport"])
+    q.execute(sql, [2, "Drone B", "Hudson Yards"])
+    q.execute(sql, [3, "Drone C", "UN Building"])
 
     #
     # Triple-quoted strings can cross lines
@@ -74,7 +74,7 @@ def populate_database():
     INSERT INTO
         bookings
     (
-        room_id, user_id, booked_on, booked_from, booked_to
+        drone_id, user_id, booked_on, booked_from, booked_to
     )
     VALUES(
         ?, ?, ?, ?, ?
@@ -87,6 +87,7 @@ def populate_database():
     q.close()
     db.commit()
     db.close()
+
 
 def get_users():
     """Get all the users from the database
@@ -103,20 +104,20 @@ def get_users():
 
     return users
 
-def get_rooms():
+def get_drones():
     """Get all the rooms from the database
     """
     db = sqlite3.connect(DATABASE_FILEPATH)
     db.row_factory = sqlite3.Row
     q = db.cursor()
 
-    q.execute("SELECT * FROM rooms")
-    rooms = q.fetchall()
+    q.execute("SELECT * FROM drones")
+    drones = q.fetchall()
 
     q.close()
     db.close()
 
-    return rooms
+    return drones
 
 def get_bookings_for_user(user_id):
     """Get all the bookings made by a user
@@ -133,14 +134,14 @@ def get_bookings_for_user(user_id):
 
     return bookings
 
-def get_bookings_for_room(user_id):
+def get_bookings_for_drone(drone_id):
     """Get all the bookings made against a room
     """
     db = sqlite3.connect(DATABASE_FILEPATH)
     db.row_factory = sqlite3.Row
     q = db.cursor()
 
-    q.execute("SELECT * FROM v_bookings WHERE user_id = ?", [user_id])
+    q.execute("SELECT * FROM v_bookings WHERE drone_id = ?", [drone_id])
     bookings = q.fetchall()
 
     q.close()
@@ -155,8 +156,8 @@ if __name__ == '__main__':
     populate_database()
     print("Bookings for user: 1")
     for booking in get_bookings_for_user(1):
-        print(booking['room_name'], booking['booked_on'])
+        print(booking['drone_name'], booking['booked_on'])
     print("Bookings for room: 2")
-    for booking in get_bookings_for_room(2):
+    for booking in get_bookings_for_drone(2):
         print(booking['user_name'], booking['booked_on'])
     print("Finished")
